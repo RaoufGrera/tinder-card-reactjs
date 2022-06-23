5e72de4358cac04fd95efd498e79ad9f6afd997b
raoufgr <raouf.grera@gmail.com>
2022-06-23 23:10:22 +0200

    add comment

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const cards = [
   'https://upload.wikimedia.org/wikipedia/commons/3/3b/Wands06.jpg'
 ]
 
-// These two are just helpers, they curate spring data, values that are later being interpolated into css
+//  These two are just helpers, they curate spring data, values that are later being interpolated into css
 const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
 const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
